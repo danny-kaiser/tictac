9231f72ebffcc118c81087afc3bf35005c092a5d
Danny <109158211+danny-kaiser@users.noreply.github.com>
2024-04-26 11:07:08 -0700

    Functional implementation using the indexes of an ordered outcome array

--- script.js
+++ script.js
@@ -15,24 +15,62 @@ function getComputerChoice () {
   }
 }
 
-function test (player) {
-  const states = ['rock', 'paper', 'scissors']
-  const winCase = [['rock', 'scissors'], ['paper', 'rock'], ['scissors', 'paper']]
-  console.log(winCase)
-}
+const choices = ['rock', 'paper', 'scissors']
+let playerScore = 0
+let computerScore = 0
 
-function playRound (playerSelection, computerSelection) {
+function playRound (player, computer = getComputerChoice()) {
+  const choice1 = choices.indexOf(player)
+  const choice2 = choices.indexOf(computer)
+  player = player.toLowerCase()
+  if (!(choices.includes(player))) {
+    return 'Invalid selection. Please choose rock, paper, or scissors!'
+  }
+  if (choice1 === choice2) {
+    return `Computer played ${computer}. It's a tie! You: ${playerScore} Computer: ${computerScore}`
+  }
+  if (choice1 === choices.length - 1 && choice2 === 0) {
+    computerScore += 1
+    return `Computer played ${computer}. You lose! You: ${playerScore} Computer: ${computerScore}`
+  }
+  if (choice2 === choices.length - 1 && choice1 === 0) {
+    playerScore += 1
+    return `Computer played ${computer}. You win! You: ${playerScore} Computer: ${computerScore}`
+  }
+  if (choice1 > choice2) {
+    playerScore += 1
+    return `Computer played ${computer}. You win! You: ${playerScore} Computer: ${computerScore}`
+  } else {
+    computerScore += 1
+    return `Computer played ${computer}. You lose! You: ${playerScore} Computer: ${computerScore}`
+  }
+}
+/*
+function playRound (playerSelection, computerSelection = getComputerChoice()) {
   const states = ['rock', 'paper', 'scissors']
   const winCase = [['rock', 'scissors'], ['paper', 'rock'], ['scissors', 'paper']]
   const player = playerSelection.toLowerCase()
   const computer = computerSelection
-  if (!(player in states)) {
+  console.log([player, computer])
+  console.log(
+    winCase.some(
+      subarr => subarr.every(
+        (arrElem, ind) => arrElem === [player, computer]
+      )
+    )
+  )
+}
+  if (!(states.includes(player))) {
     return 'Invalid selection. Please choose rock, paper, or scissors!'
   } else if (player === computer) {
     return 'It\'s a tie!'
-  } else if ([player, computer] in winCase) {
+  } else if (winCase.some(
+    subarr => subarr.every(
+      (arrElem, ind) => arrElem === [player, computer]
+    )
+  )) {
     return 'You win!'
   } else {
     return 'You lose¯\\_(ツ)_/¯'
   }
-}
+} */
