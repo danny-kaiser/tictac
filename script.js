07718a3ba976fe65c429c3d9e79687a09f74d115
Danny <109158211+danny-kaiser@users.noreply.github.com>
2024-05-04 09:36:53 -0700

    working version, fixed double score bug, added ui

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const display = document.querySelector('.display')
+const selection = document.querySelector('.selection')
+const choices = ['rock', 'paper', 'scissors']
+let playerScore = 0
+let computerScore = 0
+
 function getRandomInt () {
   const minCeiled = Math.ceil(1)
   const maxFloored = Math.floor(4)
@@ -15,62 +21,59 @@ function getComputerChoice () {
   }
 }
 
-const choices = ['rock', 'paper', 'scissors']
-let playerScore = 0
-let computerScore = 0
-
 function playRound (player, computer = getComputerChoice()) {
   const choice1 = choices.indexOf(player)
   const choice2 = choices.indexOf(computer)
   player = player.toLowerCase()
-  if (!(choices.includes(player))) {
-    return 'Invalid selection. Please choose rock, paper, or scissors!'
-  }
-  if (choice1 === choice2) {
-    return `Computer played ${computer}. It's a tie! You: ${playerScore} Computer: ${computerScore}`
-  }
-  if (choice1 === choices.length - 1 && choice2 === 0) {
+  if (!choices.includes(player)) {
+    display.textContent = 'Invalid selection. Please choose rock, paper, or scissors!'
+  } else if (choice1 === choice2) {
+    display.textContent = `Computer played ${computer}. You: ${playerScore} Computer: ${computerScore}`
+  } else if (choice1 === choices.length - 1 && choice2 === 0) {
     computerScore += 1
-    return `Computer played ${computer}. You lose! You: ${playerScore} Computer: ${computerScore}`
-  }
-  if (choice2 === choices.length - 1 && choice1 === 0) {
+    display.textContent = `Computer played ${computer}. You: ${playerScore} Computer: ${computerScore}`
+  } else if (choice2 === choices.length - 1 && choice1 === 0) {
     playerScore += 1
-    return `Computer played ${computer}. You win! You: ${playerScore} Computer: ${computerScore}`
-  }
-  if (choice1 > choice2) {
+    display.textContent = `Computer played ${computer}. You: ${playerScore} Computer: ${computerScore}`
+  } else if (choice1 > choice2) {
     playerScore += 1
-    return `Computer played ${computer}. You win! You: ${playerScore} Computer: ${computerScore}`
+    display.textContent = `Computer played ${computer}. You: ${playerScore} Computer: ${computerScore}`
   } else {
     computerScore += 1
-    return `Computer played ${computer}. You lose! You: ${playerScore} Computer: ${computerScore}`
+    display.textContent = `Computer played ${computer}. You: ${playerScore} Computer: ${computerScore}`
   }
 }
-/*
-function playRound (playerSelection, computerSelection = getComputerChoice()) {
-  const states = ['rock', 'paper', 'scissors']
-  const winCase = [['rock', 'scissors'], ['paper', 'rock'], ['scissors', 'paper']]
-  const player = playerSelection.toLowerCase()
-  const computer = computerSelection
-  console.log([player, computer])
-  console.log(
-    winCase.some(
-      subarr => subarr.every(
-        (arrElem, ind) => arrElem === [player, computer]
-      )
-    )
-  )
-}
-  if (!(states.includes(player))) {
-    return 'Invalid selection. Please choose rock, paper, or scissors!'
-  } else if (player === computer) {
-    return 'It\'s a tie!'
-  } else if (winCase.some(
-    subarr => subarr.every(
-      (arrElem, ind) => arrElem === [player, computer]
-    )
-  )) {
-    return 'You win!'
-  } else {
-    return 'You lose¯\\_(ツ)_/¯'
+
+function checkWinner () {
+  if (computerScore === 5) {
+    display.textContent = `You: ${playerScore} Computer: ${computerScore}
+    Computer Wins!`
+    computerScore = 0
+    playerScore = 0
   }
-} */
+  if (playerScore === 5) {
+    display.textContent = `You: ${playerScore} Computer: ${computerScore}
+    You Win!`
+    computerScore = 0
+    playerScore = 0
+  }
+}
+
+selection.addEventListener('click', (event) => {
+  const target = event.target
+
+  switch (target.className) {
+    case 'rock':
+      playRound('rock')
+      checkWinner()
+      break
+    case 'paper':
+      playRound('paper')
+      checkWinner()
+      break
+    case 'scissors':
+      playRound('scissors')
+      checkWinner()
+      break
+  }
+})
